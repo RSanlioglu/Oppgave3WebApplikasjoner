129f64b63a1d95755cf9d4939af5b6e9b3a625b6
Ramazan <ramazan_sanlioglu@live.com>
2021-09-11 19:43:43 +0200

    Oppgave 3 ferdig

--- Oppgave3/oppgave3.js
+++ Oppgave3/oppgave3.js
@@ -3,17 +3,73 @@ let errorCounter = 0;
 let wordPos = 0;
 let wordIndex = 0;
 
-const words = ["Conspiracy", "act", "supercalifragilisticexpialidocious"]; //Liste med ord
+const words = ["conspiracy", "act", "supercalifragilisticexpialidocious"]; //Liste med ord
 
-// TODO: Hent ut HTML #ider og knappen
-// TODO: Lag en funksjon for å skrive ut ordet som brukeren skal skrive eller en medling om at det ikke er flere ord igjen
-// TODO: Lag en funksjon som gjør at vi kan bytte ord. Må også oppdatere tellerene
-// TODO: Lag en funksjon for å sjekke om vi har skrevet riktig bokstav. Må ta hensyn til plassen i ordet vi skal skrive
-// TODO: Lag en funksjon for å sjekke om posisjonen vi er på er lik lengden på ordet vi skal skrive. Det betyr at vi er ferdig med ordet og kan bytte ord
-// TODO: Lag en funksjon som trigges når vi skriver på tastaturen og basert på hva vi skriver / posisjonen vi er på gjør nødvendige oppdateringer av grensesnittet
-// TODO: Lag en funksjon som kan brukes til å oppdatere grensesnittet
-// - Vise antall feil
-// - Vise ordet vi skal skrive
-// - Vise hvor langt vi har kommet (posisjonen) på det ordet vi skal skrive
-// TODO: Lytt til keyup på window
-// TODO: Lytt til klikk på knappen. Klikket skal gjøre knappen "disabled" samt starte oppgaven
+//Henter ut html elementer
+const wordOut = document.getElementById("word"); 
+const letterOut = document.getElementById("letter");
+const wrongOut = document.getElementById("wrongs");
+const btnStart = document.querySelector("button");
+
+//Henter ut ord og skriver den ut til bruker. Dersom det ikke er flere ord så skrives det ut en feilmelding
+const writeWord = () => {
+    (wordIndex < words.length) ? wordOut.innerHTML = words[wordIndex] : wordOut.innerHTML= `Ingen flere ord`;
+}
+
+//Funksjon for å bytte ord og endre på indexen for ordet.
+const updateWord = () => {
+    wordIndex++;
+    wordPos = 0; //Starter på 0 siden det er et nytt ord
+    writeWord();
+}
+
+//Funksjon for å sjekke om brukeren har skrevet riktig bokstav
+const checkLetter = (word, wordPos, letterOut) => {
+    return word[wordPos] === letterOut;  //Sjekker om bokstaven fra ordet stemmer i samsvar med bokstaven vi har tastet inn
+}
+
+//Funksjon som sjekker om ordet er riktig skrevet
+const checkIfWordCorrect = (word, wordPos) => {
+    return wordPos === word.length; //Dersom vi har kommet til slutten av ordet så er ordet skrevet riktig. Ellers feil
+}
+
+//Funksjon som håndterer tastetrykk. Sjekker om det er riktig bokstav eller feil bokstav.
+const handleKeyIn = ({key}) => {
+    const word = words[wordIndex]; //Henter ut bokstaven vi er i nå
+    if(checkLetter(word, wordPos, key)) { //Sjekker om bokstaven og tastetrykket er riktig
+        wordPos++;                          //Hvis riktig så går vi til neste bokstav
+        if(checkIfWordCorrect(word, wordPos)) {  //Dersom ordet vi har skrevet inn er riktig så oppdaterer vi ordet
+            updateWord();
+        }
+    } else {                                        //Ellers øker vi antall feil
+        errorCounter++;
+    }
+    updateUi(key);
+};
+
+//Funksjon som oppdaterer user interface. Tar inn en key (knapp som er tastet inn) som parameter
+const updateUi = (key) => {
+    //Dersom det er flere ord igjen fra listen så oppdaterer vi UI
+    if(wordIndex < words.length) {
+        wrongOut.innerHTML = `Antall feil: ${errorCounter}`;                     //Her vises antall feil
+        letterOut.innerHTML = `Du har tastet: ${key}`;                           //Her vises tastetrykk
+        wordOut.innerHTML = `<span class="green">${words[wordIndex].slice(       //Her vises det at du har tastet inn riktig bokstav. Bokstavene blir grønne
+            0,
+            wordPos
+          )}</span>${words[wordIndex].slice(wordPos)}`;
+    }
+}
+
+
+//Funksjon som starter applikasjonen
+const startApp = () => {
+    btnStart.disabled = true;
+    writeWord();
+}
+
+//Lytter til alle taster og håndterer dem
+window.addEventListener("keyup", handleKeyIn);
+
+//Lytter til at knappen blir klikket.
+btnStart.addEventListener("click", startApp);
+
